fc9b657e9fb6ecc30ac4952ece1edf987d5a44af
Dayeong <62468132+dayeong918@users.noreply.github.com>
2023-04-28 17:35:42 +0900

    Add files via upload

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/m/MessageToast",
+        "sap/m/MessageBox"
+    ],
+    function(BaseController,MessageToast, MessageBox) {
+      "use strict";
+  
+      return BaseController.extend("zprojectodatae0402.controller.App", {
+        onInit() {
+
+        }
+          
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zprojectodata_e04_02/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
